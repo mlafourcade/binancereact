825f366cb30b7b7ed400c6580810ef6c7a0f8f8b
mlafourcade <engenharia1@serralog.com.br>
2022-04-13 09:30:17 -0300

    Usando Contexto do react criei os temas de cores que podem ser modificados pelo botão na pagina test

--- src/shared/contexts/themeContext.js
+++ src/shared/contexts/themeContext.js
@@ -1,6 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useContext, useMemo, useState } from 'react';
+import { DarkTheme, LightTheme } from '../themes';
+
+import { ThemeProvider } from '@mui/material';
+import { Box } from '@mui/system';
 
 export const ContextData = createContext();
+export const ThemeContext = createContext();
 
 export const CustonProvider = ({ children }) => {
 
@@ -8,18 +13,65 @@ export const CustonProvider = ({ children }) => {
     const [email, setEmail] = useState();
 
     const handleData = ({ name, email }) => {
-        console.log("Dados Context", {name, email});
+        console.log('Dados Context', {name, email});
 
         setName(name);
         setEmail(email);
     }
 
     return (
-        <ContextData.Provider value={{name, email, submit:handleData}}>
-            { children }
-        </ContextData.Provider>
+        <ThemeProvider theme={LightTheme}>
+            <ContextData.Provider value={{name, email, submit:handleData}}>
+                { children }
+            </ContextData.Provider>
+        </ThemeProvider>
     )
 }
+
+export const useAppThemeContext = () => {
+    return useContext(ThemeContext);
+}
+
+export const AppThemeProvider =  ({ children }) => {
+
+    const [themeName, setThemeName] = useState('light');
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const toogleTheme = useCallback(() =>{
+        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+    }, []);
+
+    const handleData = ({ name, email }) => {
+        console.log('Dados Context', {name, email});
+
+        setName(name);
+        setEmail(email);
+    };
+
+    const theme = useMemo(() => {
+
+        console.log('Provider themeName = ', {themeName});
+
+        if (themeName === 'light')
+            return LightTheme;
+
+        return DarkTheme;
+
+    }, [themeName]);
+
+    return (
+        <ThemeContext.Provider value={{themeName, toogleTheme}}>
+            <ContextData.Provider value={{name, email, submit:handleData}}>
+                <ThemeProvider theme={theme}>
+                    <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+                        { children }
+                    </Box>
+                </ThemeProvider>
+            </ContextData.Provider>
+        </ThemeContext.Provider>
+    );
+};
 
 
 
